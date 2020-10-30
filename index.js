4fe5efd844b0a7b8b7c99367c7340c6544f7ff72
Daniel Moreno <drmjr417@sbcglobal.net>
2020-10-30 03:29:05 -0500

    Update index.js
    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const connections = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Lexy1729",
+    password: "",
     database: "employee_DB"
 });
 
